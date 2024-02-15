e298b323b2d35966d0668e63061b7166e8af9913
adrian <adrian@kelsus.com>
2024-02-15 15:16:26 -0300

    empty project

---
+++ src/test/kotlin/dummy.kt
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test
+
+class dummy {
+    @Test
+    fun sample() {
+
+    }
+}
+
+/*
+`Returns 1 if number is 1`
+`Returns 2 if number is 2`
+`Returns Fizz if number is 3`
+`Returns Buzz if number is 5`
+`Returns FizzBuzz if number is 15`
+*/
